c582783ec9330d0173c18025812dadd867f157f4
Patryk Pilek <patryk.pilek@gmail.com>
2023-02-27 23:39:38 +0000

    comments in php and practice app

---
+++ 01_First_Steps/index_3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        /*
+         * This function below displays text
+         * this is multiline comment
+         */
+        $name = "Patryk";
+        // One line comment
+        echo "Hello $name";
+    ?>
+</body>
+</html>
---
+++ practical-app/index.php
@@ -0,0 +1,36 @@
+<?php include "functions.php"; ?>
+<?php include "includes/header.php"; ?>
+
+<section class="content">
+
+    <aside class="col-sm-4">
+
+        <?php Navigation(); ?>
+
+    </aside><!--SIDEBAR-->
+
+    <article class="main-content col-sm-8">
+        <div class="jumbotron">
+            <h2 class="text-muted">Important</h2>
+
+            <h3>Personal message</h3>
+
+            <p>I hope you made it this far and are able start practicing.With this little app you can practice a small part of PHP and hopefully
+                remember some of the concepts we talked about.</p>
+
+            <p>I really enjoyed making this course for you and hopefully you appreciated it. Knowledge is a beautiful thing to have and time is
+                precious, make every minute of your life count for something.
+            </p>
+
+        </div>
+
+    </article>
+
+
+
+    <?php include "includes/footer.php"; ?>
+			
+			
+
+
+
